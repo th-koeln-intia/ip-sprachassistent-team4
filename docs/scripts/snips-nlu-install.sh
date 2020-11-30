3cd165384298bdb136beedb8df3c70c627f1771d
kevinbischof <kevin.bischof@smail.th-koeln.de>
2020-11-30 16:08:23 +0100

    Fix absolute path error, edit snips nlu install script

--- docs/scripts/snips-nlu-install.sh
+++ docs/scripts/snips-nlu-install.sh
@@ -12,21 +12,31 @@ pip install snips-nlu
 # Do Snips-NLU rebirth
 sudo apt install libatlas3-base libgfortran5
 
-cd /home/pi
+wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/scipy-1.3.3-cp37-cp37m-linux_armv7l.whl?raw=true -P $HOME/snips-nlu
+wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/scikit_learn-0.22.1-cp37-cp37m-linux_armv7l.whl?raw=true -P $HOME/snips-nlu
+wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/snips_nlu_utils-0.9.1-cp37-cp37m-linux_armv7l.whl?raw=true -P $HOME/snips-nlu
+wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/snips_nlu_parsers-0.4.3-cp37-cp37m-linux_armv7l.whl?raw=true -P $HOME/snips-nlu
+wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/snips_nlu-0.20.2-py3-none-any.whl?raw=true -P $HOME/snips-nlu
 
-wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/scipy-1.3.3-cp37-cp37m-linux_armv7l.whl?raw=true
-wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/scikit_learn-0.22.1-cp37-cp37m-linux_armv7l.whl?raw=true
-wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/snips_nlu_utils-0.9.1-cp37-cp37m-linux_armv7l.whl?raw=true
-wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/snips_nlu_parsers-0.4.3-cp37-cp37m-linux_armv7l.whl?raw=true
-wget --content-disposition https://github.com/jr-k/snips-nlu-rebirth/blob/master/wheels/snips_nlu-0.20.2-py3-none-any.whl?raw=true
+sudo pip3 install $HOME/snips-nlu/scipy-1.3.3-cp37-cp37m-linux_armv7l.whl
+sudo pip3 install $HOME/snips-nlu/scikit_learn-0.22.1-cp37-cp37m-linux_armv7l.whl
+sudo pip3 install $HOME/snips-nlu/snips_nlu_utils-0.9.1-cp37-cp37m-linux_armv7l.whl
+sudo pip3 install $HOME/snips-nlu/snips_nlu_parsers-0.4.3-cp37-cp37m-linux_armv7l.whl
+sudo pip3 install $HOME/snips-nlu/snips_nlu-0.20.2-py3-none-any.whl
 
-sudo pip3 install scipy-1.3.3-cp37-cp37m-linux_armv7l.whl
-sudo pip3 install scikit_learn-0.22.1-cp37-cp37m-linux_armv7l.whl
-sudo pip3 install snips_nlu_utils-0.9.1-cp37-cp37m-linux_armv7l.whl
-sudo pip3 install snips_nlu_parsers-0.4.3-cp37-cp37m-linux_armv7l.whl
-sudo pip3 install snips_nlu-0.20.2-py3-none-any.whl
-
+# Download language en
 snips-nlu download en
+
+# Create dirs for snips engine
 
 mkdir ~/.config/rhasspy/profiles/de/snips
 mkdir ~/.config/rhasspy/profiles/de/snips/engine
+
+# Download and start rhasspy-snips-nlu-hermes service
+sudo wget https://raw.githubusercontent.com/th-koeln-intia/ip-sprachassistent-team4/master/docs/scripts/rhasspy-snips-nlu-hermes.service -P /etc/systemd/system
+cd /etc/systemd/system
+sudo systemctl enable rhasspy-snips-nlu-hermes.service
+sudo systemctl start rhasspy-snips-nlu-hermes.service
+
+# Remove wheels
+rm -R $HOME/snips-nlu
