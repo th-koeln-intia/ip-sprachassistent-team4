a36f878a8e9337bfde6c2c68fa2992578e3193af
Lukariuss <Sh4der@users.noreply.github.com>
2021-01-12 12:39:53 +0100

    fix snips folder

--- scripts/install-rhasspy-snips-nlu_venv.sh
+++ scripts/install-rhasspy-snips-nlu_venv.sh
@@ -33,8 +33,8 @@ pip install rhasspy-snips-nlu-hermes
 snips-nlu download de
 
 # Create dirs for snips engine
-mkdir ~/.config/rhasspy/profiles/de/snips
-mkdir ~/.config/rhasspy/profiles/de/snips/engine
+mkdir -p ~/.config/rhasspy/profiles/de/snips
+mkdir -p ~/.config/rhasspy/profiles/de/snips/engine
 
 # Download and start rhasspy-snips-nlu-hermes service
 sudo wget https://raw.githubusercontent.com/th-koeln-intia/ip-sprachassistent-team4/master/scripts/rhasspy-snips-nlu-hermes_venv.service -O /etc/systemd/system/rhasspy-snips-nlu-hermes.service
